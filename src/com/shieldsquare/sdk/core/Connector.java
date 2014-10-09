a247ada2366d1f16c6f2aa8103e46f69817c98dd
nachivpn <nachivpn@gmail.com>
2014-10-09 18:48:42 +0530

    added support for replica set

--- src/com/shieldsquare/sdk/core/Connector.java
+++ src/com/shieldsquare/sdk/core/Connector.java
@@ -1,9 +1,11 @@
 /**
 Contributors: Nachi
-*/
+ */
 package com.shieldsquare.sdk.core;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
@@ -11,26 +13,27 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
 
 public class Connector {
 
 	/* Logger details */
 	private final static Logger logger = Logger.getLogger(Connector.class.getName());
-	
+
 	/* Mongo details */
-	private String mongoHost;
-	private Integer mongoPort;
-	
+
+	private List<ServerAddress> serverSeeds = new ArrayList<ServerAddress>();
+
 	/* DB details */
 	private String dbName;
 	private String dbCollectionName;
-	
+
 	/* Client variables */
 	private MongoClient mongoClient;
 	private DB db;
 	private DBCollection collection = null;
-	
-	
+
+
 	private boolean initiated = false;
 	private boolean slaveOk = false;
 	/**
@@ -41,26 +44,27 @@ public class Connector {
 	 * @param dbCollectionName
 	 */
 	public Connector(String mongoHost, Integer mongoPort, String dbName, String dbCollectionName){
-		this.mongoHost = mongoHost;
-		this.mongoPort = mongoPort;
 		this.setDbName(dbName);
 		this.setDbCollectionName(dbCollectionName);
+		addHost(mongoHost, mongoPort);
 	}
-	
-	public void initiateSession(){
 
-		try {
-			mongoClient = new MongoClient(mongoHost,mongoPort);
-			if(isSlaveOk())
-				mongoClient.setReadPreference(ReadPreference.secondaryPreferred());
-			db = mongoClient.getDB(getDbName());
-			collection = db.getCollection(getDbCollectionName());
-			initiated = true;
-		} catch (UnknownHostException uhe) {
-			logger.error("Error connecting to mongo!", uhe);
-		}
+	public void initiateSession(){
+		if(initiated)
+			closeSession();
+		mongoClient = new MongoClient(serverSeeds);
+		if(isSlaveOk())
+			mongoClient.setReadPreference(ReadPreference.secondaryPreferred());
+		db = mongoClient.getDB(getDbName());
+		collection = db.getCollection(getDbCollectionName());
+		initiated = true;
 	}
 	
+	public void closeSession(){
+		mongoClient.close();
+		mongoClient = null;
+	}
+
 	/**
 	 * 
 	 * @return mongo collection
@@ -95,5 +99,15 @@ public class Connector {
 		this.slaveOk = slaveOk;
 		return this;
 	}
-	
+
+	public Connector addHost(String mongoHost, int mongoPort){
+		try {
+			ServerAddress newSeed = new ServerAddress(mongoHost, mongoPort);
+			serverSeeds.add(newSeed);
+		} catch (UnknownHostException e) {
+			logger.error("Error connecting to mongo!" + e);
+		}
+		return this;
+	}
+
 }
